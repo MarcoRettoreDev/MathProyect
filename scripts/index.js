6ccc3b38ebab779acd1301dc32cc141b916f2b22
Marco Rettore <marcorettoremattes@gmail.com>
2022-02-08 10:41:38 -0300

    Ajustamos menu RWD index html V 1.0 actualizada

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+
+//boton menu burger
+var btnMenuBurger = document.getElementById('menu-burger');
+btnMenuBurger.onclick = () => span.style.display = "block";
+
+// Boton menu cerrar
+var span = document.getElementsByClassName("menu-wrapper")[0]; 
+// span to close the modal
+span.onclick = () => span.style.display = "none";
