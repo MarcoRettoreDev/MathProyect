12bd337f2c4b846e5158eaf2439d88c7aee24ec2
Marco Rettore <marcorettoremattes@gmail.com>
2022-02-09 22:36:01 -0300

    Terminamos CONTACT HTML V 1.0

---
+++ scripts/contact.js
@@ -0,0 +1,8 @@
+
+
+//boton menu burger
+var btnMenuBurger = document.getElementById('menu-burger');
+btnMenuBurger.onclick = () => span.style.display = "block";
+// Boton menu cerrar
+var span = document.getElementsByClassName("menu-wrapper")[0]; 
+span.onclick = () => span.style.display = "none";
