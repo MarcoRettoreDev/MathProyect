1e6adfc07a88be31ea89572f8dd141c04cd9b2ca
Marco Rettore <marcorettoremattes@gmail.com>
2022-03-28 22:27:55 -0300

    Fixed bug on paths

--- scripts/contact.js
+++
@@ -1,8 +0,0 @@
-
-
-//boton menu burger
-var btnMenuBurger = document.getElementById('menu-burger');
-btnMenuBurger.onclick = () => span.style.display = "block";
-// Boton menu cerrar
-var span = document.getElementsByClassName("menu-wrapper")[0]; 
-span.onclick = () => span.style.display = "none";
